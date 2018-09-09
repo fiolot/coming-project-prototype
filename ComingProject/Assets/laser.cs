93823f5d14fb5370ef3ed2caf5f55c2c6a40440e
Nordtwig <nordqvist.gaming@gmai.com>
2018-09-09 22:28:58 +0200

    Laser now activates on left mouse button down. Updated Laser Particle System behaviour.

---
+++ ComingProject/Assets/laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour {
+
+    public ParticleSystem laserEmitter;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            laserEmitter.Play();
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            laserEmitter.Stop();
+            //laserEmitter.Clear(); // Uncomment the code to the right to switch firing mode!
+        }
+	}
+}
